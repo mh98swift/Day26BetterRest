f5cd61de9feb74988c57f818a7a974f53124574e
mharari <mharari@varcode.com>
2022-09-12 09:33:06 +0300

    Initial Commit

---
+++ Day26BetterRest/Day26BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day26BetterRestApp.swift
+//  Day26BetterRest
+//
+//  Created by VCM1 on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day26BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
